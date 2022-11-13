d5b3f1123d226206b82db3df3b880ede681e6124
yonlu <lss.emailbox@gmail.com>
2022-11-13 12:24:09 -0500

    chore: add cors package and instantite it on server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+import cors from "cors";
 import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 import { routes } from "./routes";
@@ -5,6 +6,7 @@ import { routes } from "./routes";
 const app = express();
 app.use(express.json());
 
+app.use(cors());
 app.use(routes);
 
 app.use(
