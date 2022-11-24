55292706a7c69311b43c5fff7b4d2f15ce480838
yonlu <lss.emailbox@gmail.com>
2022-11-24 09:20:16 -0500

    docs: WIP - add swagger

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import "reflect-metadata";
 import cors from "cors";
 import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
+import swaggerUi from "swagger-ui-express";
+import swaggerFile from "./swagger.json";
 import { routes } from "./routes";
 import "./shared/container";
 import uploadConfig from "./config/upload";
@@ -10,6 +12,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(routes);
 app.use("/files/nikkes", express.static(uploadConfig.nikkes.directory));
 
